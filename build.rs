14d783b36ab8859e0b68f3a513305f7a91413e1e
Tyler Slabinski <tslabinski@slabity.net>
2016-08-21 19:18:56 -0400

    Link dynamically to libgbm

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 extern crate bindgen;
 
 fn generate_shim_bindings() {
-    let builder = bindgen::Builder::new("src/ffi/cc/gbm_shim.c");
+    let mut builder = bindgen::Builder::new("src/ffi/cc/gbm_shim.c");
+    builder.link("gbm", bindgen::LinkType::Dynamic);
     match builder.generate() {
         Ok(b) => b.write_to_file("src/ffi/gbm_shim.rs").unwrap(),
         Err(e) => panic!(e)
