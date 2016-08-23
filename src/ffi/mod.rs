dfea8450d7eddf3899c0a30d50599078de3e4da7
Tyler Slabinski <tslabinski@slabity.net>
2016-08-23 15:37:39 -0400

    Update FFI to track errors and get errno

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,9 +1,25 @@
 mod gbm_shim;
 
+use errno::{Errno, errno, set_errno};
+
 pub use self::gbm_shim::*;
 use std::os::unix::io::RawFd;
 use std::mem::transmute;
 use std::os::raw::c_void;
+
+
+macro_rules! gbm_cmd {
+    ( $func:expr ) => ( unsafe {
+        set_errno(Errno(0));
+        let ptr = $func;
+        if ptr.is_null() {
+            let err = errno();
+            panic!("{}", err);
+        }
+        ptr
+    })
+}
+
 
 #[derive(Debug)]
 pub struct GbmDevice {
@@ -12,10 +28,7 @@ pub struct GbmDevice {
 
 impl GbmDevice {
     pub fn new(fd: RawFd) -> GbmDevice {
-        let ptr = unsafe {
-            gbm_create_device(fd)
-        };
-
+        let ptr = gbm_cmd!(gbm_create_device(fd));
         GbmDevice {
             raw: ptr
         }
@@ -24,9 +37,7 @@ impl GbmDevice {
 
 impl Drop for GbmDevice {
     fn drop(&mut self) {
-        unsafe {
-            gbm_device_destroy(self.raw);
-        }
+        unsafe { gbm_device_destroy(self.raw) };
     }
 }
 
@@ -37,37 +48,27 @@ pub struct GbmSurface {
 
 impl GbmSurface {
     pub fn new(device: &GbmDevice, width: u32, height: u32, format: u32, flags: u32) -> GbmSurface {
-        let ptr = unsafe {
-            gbm_surface_create(device.raw, width, height, format, flags)
-        };
-
+        let ptr = gbm_cmd!(gbm_surface_create(device.raw, width, height, format, flags));
         GbmSurface {
             raw: ptr
         }
     }
 
     pub fn lock_front_buffer(&self) -> GbmBufferObject {
-        let ptr = unsafe {
-            gbm_surface_lock_front_buffer(self.raw)
-        };
-
+        let ptr = gbm_cmd!(gbm_surface_lock_front_buffer(self.raw));
         GbmBufferObject {
             raw: ptr
         }
     }
 
     pub fn release_front_buffer(&self, buffer: GbmBufferObject) {
-        unsafe {
-            gbm_surface_release_buffer(self.raw, buffer.raw);
-        }
+        unsafe { gbm_surface_release_buffer(self.raw, buffer.raw) };
     }
 }
 
 impl Drop for GbmSurface {
     fn drop(&mut self) {
-        unsafe {
-            gbm_surface_destroy(self.raw);
-        }
+        unsafe { gbm_surface_destroy(self.raw) };
     }
 }
 
@@ -78,43 +79,30 @@ pub struct GbmBufferObject {
 
 impl GbmBufferObject {
     pub fn new(device: &GbmDevice, width: u32, height: u32, format: u32, flags: u32) -> GbmBufferObject {
-        let ptr = unsafe {
-            gbm_bo_create(device.raw, width, height, format, flags)
-        };
-
+        let ptr = gbm_cmd!(gbm_bo_create(device.raw, width, height, format, flags));
         GbmBufferObject {
             raw: ptr
         }
     }
 
     pub fn width(&self) -> u32 {
-        unsafe {
-            gbm_bo_get_width(self.raw)
-        }
+        unsafe { gbm_bo_get_width(self.raw) }
     }
 
     pub fn height(&self) -> u32 {
-        unsafe {
-            gbm_bo_get_height(self.raw)
-        }
+        unsafe { gbm_bo_get_height(self.raw) }
     }
 
     pub fn stride(&self) -> u32 {
-        unsafe {
-            gbm_bo_get_stride(self.raw)
-        }
+        unsafe { gbm_bo_get_stride(self.raw) }
     }
 
     pub fn format(&self) -> u32 {
-        unsafe {
-            gbm_bo_get_format(self.raw)
-        }
+        unsafe { gbm_bo_get_format(self.raw) }
     }
 
-    pub fn handle(&self) -> u64 {
-        unsafe {
-            transmute(gbm_bo_get_handle(self.raw))
-        }
+    pub fn handle(&self) -> *mut c_void {
+        unsafe { transmute(gbm_bo_get_handle(self.raw)) }
     }
 
     pub unsafe fn set_user_data(&self, data: *mut c_void) {
@@ -124,9 +112,7 @@ impl GbmBufferObject {
 
 impl Drop for GbmBufferObject {
     fn drop(&mut self) {
-        unsafe {
-            gbm_bo_destroy(self.raw);
-        }
+        unsafe { gbm_bo_destroy(self.raw) };
     }
 }
 
